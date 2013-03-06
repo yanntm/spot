1084c3d35ecdbafba92e7982406d982f0d66186e
Etienne Renault <renault@lrde.epita.fr>
2013-03-06 16:57:30 +0100

    Labelling SCC tools
    This commit povides some labelling tools: all
    terminal SCC will have a [T] acceptance set,
    all weak a [W] one and all strong a [S] one.
    
    * src/tgbaalgos/Makefile.am: add new files
    * src/tgbaalgos/accpostproc.cc: new implem file
    * src/tgbaalgos/accpostproc.hh: new header file

---
+++ src/tgbaalgos/accpostproc.cc
@@ -0,0 +1,176 @@
+// Copyright (C) 2013 Laboratoire de Recherche et Developpement de
+// l'Epita (LRDE).
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "accpostproc.hh"
+#include "tgbaalgos/reachiter.hh"
+#include "tgba/tgbaexplicit.hh"
+#include "tgbaalgos/dotty.hh"
+#include "ltlast/constant.hh"
+#include "tgbaalgos/minimize.hh"
+#include "tgbaalgos/simulation.hh"
+#include "tgbaalgos/save.hh"
+#include "tgbaalgos/scc.hh"
+#include "ltlenv/environment.hh"
+#include "ltlenv/defaultenv.hh"
+
+namespace spot
+{
+
+  namespace
+  {
+    // Tool function used to facilitate the creation of the automaton
+    // for being Generic
+    static
+    state_explicit_number::transition*
+    create_transition(const tgba*, tgba_explicit_number* out_aut,
+		      const state*, int in, const state*, int out)
+    {
+      return out_aut->create_transition(in, out);
+    }
+
+
+    class relabel_iter: public tgba_reachable_iterator_depth_first
+    {
+    public:
+      relabel_iter(const spot::tgba* a,
+		   const spot::scc_map& sm,
+		   ltl::declarative_environment& env)
+	: tgba_reachable_iterator_depth_first(a),
+	  out_(new  spot::tgba_explicit_number(a->get_dict())),
+	  sm_(sm),
+	  env_(env)
+      {
+	// Preconditions to apply algo
+	// assert(a->number_of_acceptance_conditions() == 1);
+	// assert (a->all_acceptance_conditions() != bddtrue);
+
+	// Register variable
+	const ltl::formula* f = env_.require("[S]");
+	int v = out_->get_dict()
+	  ->register_acceptance_variable (f, out_);
+	strong_acc = bdd_ithvar(v);
+	f->destroy();
+
+	f = env_.require("[W]");
+	v = out_->get_dict()
+	  ->register_acceptance_variable (f, out_);
+	weak_acc = bdd_ithvar(v);
+	f->destroy();
+
+	f = env_.require("[T]");
+	v = out_->get_dict()
+	  ->register_acceptance_variable (f, out_);
+	terminal_acc = bdd_ithvar(v);
+	f->destroy();
+
+	all_cond = a->all_acceptance_conditions();
+	// assert( all_cond != bddfalse);
+	// assert( all_cond != bddtrue);
+      }
+
+      spot::tgba*
+      result()
+      {
+	return out_;
+      }
+
+      bool
+      want_state(const state*) const
+      {
+	// we want to walk accross all the product
+	// so we allways want a state
+	return true;
+      }
+
+      void
+      process_link(const state* in_s, int in,
+		   const state* out_s, int out,
+		   const tgba_succ_iterator* si)
+      {
+	state_explicit_number::transition* t =
+	  create_transition(this->aut_, out_, in_s, in, out_s, out);
+	out_->add_conditions(t, si->current_condition());
+	out_->add_acceptance_conditions
+	  (t, si->current_acceptance_conditions());
+
+
+	// In the case of a strong / strong transition
+	// we just have to add one more acceptance
+	// it's the StrongScc acceptance label
+	int scc_in = sm_.scc_of_state (in_s);
+	int scc_out = sm_.scc_of_state (out_s);
+	if (scc_in == scc_out && sm_.strong(scc_out))
+	  {
+	    out_->add_acceptance_conditions (t, strong_acc);
+	  }
+	else if (scc_in == scc_out && sm_.terminal_accepting(scc_out))
+	  {
+	    out_->add_acceptance_conditions (t, terminal_acc);
+	    out_->add_acceptance_conditions (t, all_cond);
+	  }
+	else if (scc_in == scc_out && sm_.weak_accepting(scc_out))
+	  {
+	    out_->add_acceptance_conditions (t, weak_acc);
+	    out_->add_acceptance_conditions (t, all_cond);
+	  }
+      }
+
+    private:
+      spot::tgba_explicit_number* out_;
+      const scc_map& sm_;
+      bdd strong_acc;
+      bdd weak_acc;
+      bdd terminal_acc;
+      bdd  all_cond;
+      ltl::declarative_environment& env_;
+    };
+  }
+
+  const tgba* add_fake_acceptance_condition (const tgba* a,
+					      ltl::declarative_environment* env,
+					      spot::scc_map* sm)
+  {
+    spot::scc_map* x = sm;
+
+    // Create the map if needed
+    if (!sm)
+      {
+	x = new spot::scc_map(a);
+	x->build_map();
+      }
+
+    relabel_iter ri  (a, *x, *env);
+    ri.run();
+
+    if (!sm)
+      delete x;
+
+    return ri.result();
+  }
+
+  ltl::declarative_environment* create_env_acc()
+  {
+    ltl::declarative_environment* envacc =
+      new spot::ltl::declarative_environment ();
+    envacc->declare("[S]");
+    envacc->declare("[W]");
+    envacc->declare("[T]");
+    return envacc;
+  }
+
+}
---
+++ src/tgbaalgos/accpostproc.hh
@@ -0,0 +1,57 @@
+// Copyright (C) 2013 Laboratoire de Recherche et Developpement de
+// l'Epita (LRDE).
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SPOT_TGBAALGOS_ACCPOSTPROC_HH
+# define SPOT_TGBAALGOS_ACCPOSTPROC_HH
+
+#include "tgba/tgba.hh"
+#include "tgbaalgos/scc.hh"
+#include "ltlenv/declenv.hh"
+
+namespace spot
+{
+
+  /// \addtogroup tgba_reduction
+  /// @{
+
+
+  /// Returnes a TGBA wich is the same than the original one but
+  /// where other acceptance conditions have been added
+  ///
+  /// Warning: All emptiness check algorithms are not able to deal
+  /// with such automaton, and moreover sccmap can detect these
+  /// acceptance condition as useless and remove it
+  ///
+  /// This method add a fake transition condition on all Strong SCC
+  /// Weak SCC and Termninal SCC
+  ///
+  /// Note that env used here must have been declared by create_env_acc
+  /// or provide a characterisation for Terminal Weak and strong SCC
+  const tgba* add_fake_acceptance_condition (const tgba *a,
+					      ltl::declarative_environment* env,
+					      spot::scc_map* sm = 0);
+
+
+  /// Create an environment which is able to provide three acceptance
+  /// conditions in order to tag all scc
+  ltl::declarative_environment* create_env_acc ();
+
+  /// @}
+}
+
+#endif // SPOT_TGBAALGOS_ACCPOSTPROC_HH
