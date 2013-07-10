0671751e903a711f8701a94f60daad7a4e777cc3
Etienne Renault <renault@lrde.epita.fr>
2013-07-10 11:26:24 +0200

    Instrument DijkstraSCC
    * src/fasttgbaalgos/ec/dijkstra_scc.cc: Update Implem.
    * src/fasttgbaalgos/ec/dijkstra_scc.hh: Update Interface.

--- src/fasttgbaalgos/ec/dijkstra_scc.cc
+++ src/fasttgbaalgos/ec/dijkstra_scc.cc
@@ -39,7 +39,9 @@ namespace spot
     update_loop_cpt_(0),
     roots_poped_cpt_(0),
     states_cpt_(0),
-    transitions_cpt_(0)
+    transitions_cpt_(0),
+    memory_cost_(0),
+    trivial_scc_(0)
   {
     a_ = inst->get_automaton ();
     if (!option.compare("-cs-ds"))
@@ -114,6 +116,11 @@ namespace spot
 
     roots_stack_->push_trivial(todo.size() -1);
 
+    int tmp_cost = 2*roots_stack_->size() + 2*H.size() + 1*live.size()
+      + (deadstore_? deadstore_->size() : 0);
+    if (tmp_cost > memory_cost_)
+      memory_cost_ = tmp_cost;
+
   }
 
   void dijkstra_scc::dfs_pop()
@@ -129,8 +136,10 @@ namespace spot
       {
 	++roots_poped_cpt_;
 	roots_stack_->pop();
+	int trivial = 0;
 	while (live.size() > steppos)
 	  {
+	    ++trivial;
 	    if (deadstore_)	// There is a deadstore
 	      {
 		deadstore_->add(live.back());
@@ -144,6 +153,8 @@ namespace spot
 		live.pop_back();
 	      }
 	  }
+	if (trivial == 1) // we just popped a trivial
+	  ++trivial_scc_;
       }
   }
 
@@ -263,6 +274,10 @@ namespace spot
       + std::to_string(transitions_cpt_)
       + ","
       + std::to_string(states_cpt_)
+      + ","
+      + std::to_string(memory_cost_)
+      + ","
+      + std::to_string(trivial_scc_)
       ;
   }
 }
