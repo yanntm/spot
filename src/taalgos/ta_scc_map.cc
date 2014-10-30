8fb32cc01e617f7361623a07b1c5636b87718963
Etienne Renault <renault@lrde.epita.fr>
2014-10-30 14:50:21 +0100

    Compute SCCs of a TA
    * src/taalgos/ta_scc_map.hh: Interface.
    * src/taalgos/ta_scc_map.cc: Implementation.

---
+++ src/taalgos/ta_scc_map.cc
@@ -0,0 +1,199 @@
+// -*- coding: utf-8 -*-
+// Copyright (C) 2014 Laboratoire de Recherche et Développement
+// de l'Epita (LRDE).
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "ta/ta.hh"
+#include "ta_scc_map.hh"
+#include <iosfwd>
+
+namespace spot
+{
+  ta_scc_map::ta_scc_map(const ta* t):
+    t_(t),
+    transitions_(0),
+    states_(0),
+    sccs_(0),
+    id_(0)
+  {
+    build_map();
+  }
+
+  ta_scc_map::~ta_scc_map()
+  {
+    seen_map::const_iterator s = seen.begin();
+    while (s != seen.end())
+      {
+        // Advance the iterator before deleting the "key" pointer.
+        const state* ptr = s->first;
+        ++s;
+        t_->free_state(ptr);
+      }
+  }
+
+  void ta_scc_map::dfs_push(const spot::state* st)
+  {
+    dfs_element dfs_elt = {st, 0};
+    todo.push_back(dfs_elt);
+    ++id_;
+    seen[st] = id_;
+    lowlink_stack.push_back(id_);
+    ++states_;
+    live_stack.push_back(st);
+  }
+
+  void ta_scc_map::dfs_pop()
+  {
+    const state* st = todo.back().st;
+    delete todo.back().si;
+    todo.pop_back();
+
+    int  ll = lowlink_stack.back();
+    lowlink_stack.pop_back();
+    if (ll == seen[st])
+      {
+	// Root !
+	scc_info i = {false, false, true};
+	infos.push_back(i);
+
+	int scc_size = 0;
+	while (live_stack.size() != todo.size())
+	  {
+	    ++scc_size;
+	    const state* state = live_stack.back();
+	    live_stack.pop_back();
+
+	    // Now a state is associate to its (negated) scc number
+	    seen[state] = -sccs_;
+
+	    // Check wheter the state is livelock
+	    if (t_->is_livelock_accepting_state(state))
+	      infos.back().contains_livelock_acc_state = true;
+
+	    if (t_->is_accepting_state(state))
+	      infos.back().contains_buchi_acc_state = true;
+	  }
+	if (scc_size > 1)
+	  infos.back().is_trivial = false;
+
+	// Finally increment the scc number!
+	++sccs_;
+      }
+    else
+      {
+	lowlink_stack.back() = std::min(ll, lowlink_stack.back());
+      }
+
+  }
+
+  void ta_scc_map::dfs_update(const state* src, const state* dst)
+  {
+    int ll_src = seen[src];
+    int ll_dst = seen[dst];
+    lowlink_stack.back() = std::min(ll_src, ll_dst);
+  }
+
+  void ta_scc_map::build_map()
+  {
+
+    // Get the artificial initial state
+    spot::state* artificial_initial_state = t_->get_artificial_initial_state();
+
+    // Support only TA with artificial state
+    if (artificial_initial_state == 0)
+      assert(false);
+
+    dfs_push(artificial_initial_state);
+
+    // Perform the DFS
+    while (!todo.empty())
+      {
+	if (todo.back().si == 0)
+	  {
+	    todo.back().si = t_->succ_iter(todo.back().st);
+	    todo.back().si->first();
+	  }
+	else
+	  {
+	    todo.back().si->next();
+	  }
+
+	if (todo.back().si->done())
+	  {
+	    dfs_pop();
+	  }
+	else
+	  {
+	    ++transitions_;
+	    const state* current = todo.back().si->current_state();
+	    seen_map::const_iterator s = seen.find(current);
+            if (s == seen.end())
+              {
+	    	dfs_push(current);
+	    	continue;
+	      }
+	    else
+	      {
+	    	// Check if alive!
+		if (seen[current] > 0)
+		  dfs_update(todo.back().st, current);
+	      }
+	    current->destroy();
+	  }
+      }
+  }
+
+  unsigned ta_scc_map::transitions()
+  {
+    return transitions_;
+  }
+
+  unsigned ta_scc_map::states()
+  {
+    return states_;
+  }
+
+  unsigned ta_scc_map::sccs()
+  {
+    return sccs_;
+  }
+
+  void ta_scc_map::dump_infos()
+  {
+    std::cout << "Transitions : " << transitions() << std::endl;
+    std::cout << "States : " << states() << std::endl;
+    std::cout << "SCCs : " << sccs() << std::endl;
+
+    for (unsigned int i = 0; i < infos.size(); ++i)
+      {
+	std::cout << "SCC " << i << ":" <<  std::endl;
+	std::cout << "    contains_livelock_acc_state : "
+		  << infos[i].contains_livelock_acc_state << std::endl;
+	std::cout << "    contains_buchi_acc_state    : "
+		  << infos[i].contains_buchi_acc_state << std::endl;
+	std::cout << "    is_trivial : " << infos[i].is_trivial << std::endl;
+      }
+
+  }
+
+  void ta_build_scc_map(const ta* t)
+  {
+    ta_scc_map* sccmap = new ta_scc_map(t);
+    sccmap->dump_infos();
+    delete sccmap;
+  }
+}
---
+++ src/taalgos/ta_scc_map.hh
@@ -0,0 +1,126 @@
+// -*- coding: utf-8 -*-
+// Copyright (C) 2014 Laboratoire de Recherche et Développement
+// de l'Epita (LRDE).
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SPOT_TAALGOS_TA_SCC_MAP_HH
+# define SPOT_TAALGOS_TA_SCC_MAP_HH
+
+#include "ta/ta.hh"
+#include <iosfwd>
+
+namespace spot
+{
+
+  /// \addtogroup ta_misc
+  /// @{
+
+  /// \brief This class computes the SCC of the TA automaton
+  class SPOT_API ta_scc_map
+  {
+  public:
+    /// \brief Simple constructor
+    ta_scc_map(const ta* t);
+
+    /// \brief Simple Destructor
+    virtual ~ta_scc_map();
+
+    /// \brief Number of transitions in the TA
+    unsigned transitions();
+
+    /// \brief Number of states in the TA
+    unsigned states();
+
+    /// \brief Number of scc in the automaton
+    unsigned sccs();
+
+    void dump_infos();
+
+  private:
+    /// \brief Construct the SCC map of the TA automaton using a
+    // Tarjan-like algorithm
+    void build_map();
+
+    /// \brief The TA considered
+    const ta* t_;
+
+    /// \brief Count the number of transitions
+    unsigned transitions_;
+
+    /// \brief Count the number of states
+    unsigned states_;
+
+    /// \brief Count the number of sccs
+    unsigned sccs_;
+
+    /// \brief The informations associated to an SCC
+    struct scc_info
+    {
+      bool contains_livelock_acc_state;
+      bool contains_buchi_acc_state;
+      bool is_trivial;
+    };
+
+    /// \brief The informations of each SCCs
+    std::vector<scc_info> infos;
+
+    // -------------------------------------------------------
+    // This part contains variables used to perform the scc
+    // computation
+    // -------------------------------------------------------
+
+    /// \brief Element of the DFS stack
+    struct dfs_element
+    {
+      const state* st;
+      ta_succ_iterator* si;
+    };
+
+    /// \brief The DFS stack
+    std::vector<dfs_element> todo;
+
+    /// \brief Uniq identifier for each state during DFS
+    int id_;
+
+    /// \brief Call when a new state is discovered
+    void dfs_push(const spot::state*);
+
+    /// \brief Call when a state has been fully explored
+    void dfs_pop();
+
+    /// \brief Call when a closing edge is detected
+    void dfs_update(const spot::state*, const spot::state*);
+
+    /// \brief The lowlink stack
+    std::vector<int> lowlink_stack;
+
+    /// \brief The live stack (could integrate Nuutila's optimisation)
+    std::vector<const state*> live_stack;
+
+    /// \brief The map that will contain all states.
+    typedef Sgi::hash_map<const state*, int, state_ptr_hash, state_ptr_equal>
+        seen_map;
+    seen_map seen;
+  };
+
+  /// \brief Compute statistics for an automaton.
+  SPOT_API void ta_build_scc_map(const ta* t);
+
+  /// @}
+}
+
+#endif // SPOT_TAALGOS_TA_SCC_MAP_HH
