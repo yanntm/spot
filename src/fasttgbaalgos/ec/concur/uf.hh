f64bfc757aff8242c3756fe2b7b7d241f9fd06d7
Etienne Renault <renault@lrde.epita.fr>
2015-07-23 12:45:54 +0200

    Bug fixes
    Resolve path compression, locking
    twice the same element and update markset.
    
    * src/fasttgbaalgos/ec/concur/uf.hh: here.

--- src/fasttgbaalgos/ec/concur/uf.hh
+++ src/fasttgbaalgos/ec/concur/uf.hh
@@ -65,7 +65,6 @@ fasttgba_state_ptrint_hash_wrapper(void *elt, void *ctx)
 void *
 fasttgba_state_ptrint_clone_wrapper(void *key, void *ctx)
 {
-  // FIXME?
   assert(key);
   return key;
   (void)ctx;
@@ -75,8 +74,6 @@ void
 fasttgba_state_ptrint_free_wrapper(void *elt)
 {
   assert(elt);
-  //const spot::fasttgba_state* l = (const spot::fasttgba_state*)elt;
-  //l->destroy();
   (void) elt;
 }
 
@@ -88,79 +85,6 @@ static const datatype_t DATATYPE_INT_PTRINT =
     fasttgba_state_ptrint_clone_wrapper,
     fasttgba_state_ptrint_free_wrapper
   };
-
-
-
-
-
-
-
-
-
-
-// ------------------------------------------------------------
-
-static int
-sharedop_cmp_wrapper(void *hash1, void *hash2)
-{
-  const shared_op* l = (const shared_op*)hash1;
-  const shared_op* r = (const shared_op*)hash2;
-  assert(l && r);
-  return 0 /*FIXME l->hash_ - r->hash_*/;
-
-  // if (l->acc_ != r->acc_)
-  //   return l->acc_ - r->acc_;
-  // const spot::fasttgba_state* s11 = (const spot::fasttgba_state*)l->arg1_;
-  // const spot::fasttgba_state* s12 = (const spot::fasttgba_state*)r->arg1_;
-  // // if (l->op_ == makedead)
-  // //   return s11->compare(s12);
-
-  // const spot::fasttgba_state* s21 = (const spot::fasttgba_state*)l->arg2_;
-  // const spot::fasttgba_state* s22 = (const spot::fasttgba_state*)r->arg2_;
-
-  // int retval = s11->compare(s12);
-  // int retval2 = s21->compare(s22);
-  // return  (l->acc_ - r->acc_) + retval + retval2 ? s21->compare(s12) + s11->compare(s22): 0;
-}
-
-uint32_t
-sharedop_hash_wrapper(void *elt, void *ctx)
-{
-  const shared_op* e = (const shared_op*)elt;
-  (void) e;
-  return 0 /* FIXME e->hash_*/;
-  (void)ctx;
-}
-
-void *
-sharedop_clone_wrapper(void *key, void *ctx)
-{
-  // FIXME?
-  assert(key);
-  return key;
-  (void)ctx;
-}
-
-void
-sharedop_free_wrapper(void *elt)
-{
-  assert(elt);
-  (void) elt;
-}
-
-const size_t INIT_SCALE_SHAREDOP = 12;
-static const datatype_t DATATYPE_SHAREDOP =
-  {
-    sharedop_cmp_wrapper,
-    sharedop_hash_wrapper,
-    sharedop_clone_wrapper,
-    sharedop_free_wrapper
-  };
-
-// ------------------------------------------------------------
-
-
-
 
 
 namespace spot
@@ -208,32 +132,11 @@ namespace spot
     uf_lock_free(int thread_number) : thread_number_(thread_number), size_(0)
     {
       effective_uf = uf_alloc(&DATATYPE_INT_PTRINT, INIT_SCALE, thread_number);
-      mem_table_ = ht_alloc (&DATATYPE_SHAREDOP, INIT_SCALE_SHAREDOP);
-      prealloc_size_ = 1000;
-      prealloc_ = new shared_op*[thread_number_];
-      for (int i = 0; i < thread_number_; ++i)
-	{
-	  prealloc_ [i] = new shared_op[prealloc_size_+1];
-	  prealloc_ [i][prealloc_size_].arg1_ = 0;
-	  prealloc_first_.push_back(prealloc_[i]);
-	  prealloc_idx_.push_back(0);
-	}
     }
 
     virtual ~uf_lock_free()
     {
       uf_free (effective_uf, size_ /*FIXME*/, thread_number_);
-      for (int i = 0; i < thread_number_; ++i)
-      	{
-      	  shared_op* tmp =  prealloc_first_[i];
-      	  while (tmp)
-      	    {
-      	      shared_op* old = tmp;
-      	      tmp = (shared_op*)tmp[prealloc_size_].arg1_;
-	      delete[] old;
-      	    }
-      	}
-      delete[] prealloc_;
     }
 
     /// \brief insert a new element in the union find
@@ -250,58 +153,12 @@ namespace spot
       return inserted;
     }
 
-    // inline bool try_insert(enum op_type op,
-    // 		    const fasttgba_state* left,
-    // 		    const fasttgba_state* right,
-    // 		    unsigned long acc,
-    // 		    int tn)
-    // {
-    //   shared_op* node = 0;
-    //   if(prealloc_idx_[tn] < prealloc_size_)
-    // 	{
-    // 	  node = &prealloc_[tn][prealloc_idx_[tn]];
-    // 	  ++prealloc_idx_[tn];
-    // 	}
-    //   else
-    // 	{
-    // 	  // End of the prealloc, use the last room to
-    // 	  // store pointer to a new prealloc range.
-    // 	  node = new shared_op[prealloc_size_+1];
-    // 	  prealloc_[tn][prealloc_size_].arg1_ = (void*) node;
-    // 	  prealloc_idx_[tn] = 1; // since node refer prealloc_[0]
-    // 	  prealloc_[tn] = node;
-    // 	  prealloc_[tn][prealloc_size_].arg1_ = 0;
-    // 	}
-    //   node->op_ = op;
-    //   node->arg1_ = (void*)left;
-    //   node->arg2_ = (void*)right;
-    //   node->acc_ = acc;
-    //   if (node->op_ == makedead)
-    // 	node->hash_ = left->hash() + acc;
-    //   else
-    // 	node->hash_ = left->hash() + right->hash() + acc;
-
-    //   map_key_t clone = 0;
-    //   if (ht_cas_empty (mem_table_, (map_key_t)node,
-    // 	  		(map_val_t)1, &clone, (void*)NULL)
-    // 	  == DOES_NOT_EXIST)
-    // 	return true;
-
-    //   // Insertion failed;
-    //   --prealloc_idx_[tn];
-    //   return false;
-    // }
-
-
     /// \brief Mark an elemnent as dead
     virtual void make_dead(const fasttgba_state* key, int tn)
     {
       // The Concurent Hash Map doesn't support key == 0 because
       // it represent a special tag.
       assert(key);
-
-      // if (!try_insert(makedead, key, 0, 0, tn))
-      // 	return;
       (void)tn;
       uf_node_t* node = (uf_node_t*)
 	ht_get(effective_uf->table, (map_key_t) key);
@@ -320,11 +177,8 @@ namespace spot
 			  const fasttgba_state* right,
 			  const markset acc, bool *is_dead, int tn)
     {
-      unsigned long tmp = acc.to_ulong();
-
-      // if(!try_insert(op_type::unite, left, right, tmp, tn))
-      // 	return acc;
       (void)tn;
+      unsigned long tmp = acc.to_ulong();
       uf_node_t* l = (uf_node_t*)
 	ht_get(effective_uf->table, (map_key_t) left);
       uf_node_t* r = (uf_node_t*)
@@ -333,10 +187,6 @@ namespace spot
 
       uf_node_t* fast_ret = uf_unite (effective_uf, l, r, &ret_acc);
       *is_dead = fast_ret == effective_uf->dead_;
-
-
-      // printf("%s %zu %zu\n", acc.dump().c_str(), tmp, ret_acc);
-      // The markset is empty don't need to go further
 
       if (!tmp || *is_dead || ret_acc == tmp)
 	return acc;
@@ -376,7 +226,6 @@ namespace spot
 
     virtual unsigned long make_and_unite(const fasttgba_state* left,
 			   const fasttgba_state* right,
-			   //const markset acc,
 			   unsigned long tmp, bool *is_dead,
 			   int tn)
     {
@@ -391,14 +240,9 @@ namespace spot
       if (inserted)
 	++size_;
 
-
       unsigned long ret_acc = 0;
       uf_node_t* fast_ret = uf_unite (effective_uf, l, r, &ret_acc);
       *is_dead = fast_ret == effective_uf->dead_;
-
-      //      unsigned long tmp = acc.to_ulong();
-      // printf("%s %zu %zu\n", acc.dump().c_str(), tmp, ret_acc);
-      // The markset is empty don't need to go further
 
       if (!tmp || *is_dead || ret_acc == tmp)
 	return tmp;
@@ -408,13 +252,6 @@ namespace spot
       if (!res)
 	return 0;
       return res;
-
-
-      // // if res equal 0 the state is dead!
-      // // The add doesn't change anything
-      // if (!res || tmp == res)
-      // 	return acc;
-      // return acc | res;
     }
 
     /// \brief Mark an elemnent as dead
@@ -440,11 +277,6 @@ namespace spot
 
   private:
     uf_t* effective_uf;		///< \brief the union find
-    hashtable_t *mem_table_;
-    shared_op  **prealloc_;
-    std::vector<int> prealloc_idx_;
-    std::vector<shared_op*> prealloc_first_;
-    int prealloc_size_;
     int thread_number_;
     std::atomic<int> size_;
   };
@@ -465,7 +297,6 @@ namespace spot
     uf_fine_grain
     (int thread_number) : thread_number_(thread_number), size_(0)
     {
-      effective_uf = uf_alloc(&DATATYPE_INT_PTRINT, INIT_SCALE, thread_number);
       visited_ = ht_alloc(&DATATYPE_INT_PTRINT, INIT_SCALE);
       for (int i = 0 ; i < thread_number_; ++i)
 	{
@@ -518,8 +349,8 @@ namespace spot
     node* find(node* n)
     {
       assert(n);
-      node* p = n->parent;
-      while (p != n)
+      node* p = n;
+      while (p != p->parent)
 	{
 	  p = p->parent;
 	}
@@ -536,10 +367,9 @@ namespace spot
       while(true)
 	{
 	  node->is_dead = true;
-	  struct node* tmp = find(node);
-	  if (tmp == node)
+	  node = find(node);
+	  if (node == node->parent)
 	    break;
-	  node = tmp;
 	}
     }
 
@@ -562,15 +392,48 @@ namespace spot
       struct node* l = (struct node*) ht_get(visited_, (map_key_t) left);
       struct node* r = (struct node*) ht_get(visited_, (map_key_t) right);
 
-      struct node *xl, *xr;
+      struct node *xl, *xr, *xtmp;
 
       while(true)
 	{
 	  xl = find(l);
 	  xr = find(r);
- 	  xl->mutex.lock();
+
+	  // The two elements are already in the same partition
+	  // Consider a special case since locking the same mutex
+	  // twice leads to an undefined behavior.
+	  if (xl == xr)
+	    {
+	      if (tmp == 0)
+		return acc;
+
+	      // update only markset;
+	      while (true)
+		{
+		  xl->mutex.lock();
+		  if (xl->parent == xl)
+		    {
+		      tmp = tmp | xl->markset;
+		      xl->markset = tmp;
+
+		      // Compress
+		      while (l->parent != xl)
+			{
+			  xtmp = l->parent;
+			  l->parent = xl;
+			  l = xtmp;
+			}
+		      xl->mutex.unlock();
+		      return acc | tmp;
+		    }
+		  xl->mutex.unlock();
+		  xl = find(l);
+		}
+	    }
+
+	  xl->mutex.lock();
 	  xr->mutex.lock();
-	  if (xl->parent == xl && xr->parent == r)
+	  if (xl->parent == xl && xr->parent == xr)
 	    break;
 	  xr->mutex.unlock();
 	  xl->mutex.unlock();
@@ -579,27 +442,37 @@ namespace spot
 
       // compress path
       while (l->parent != xl)
-	l->parent = xl;
+	{
+	  xtmp = l->parent;
+	  l->parent = xl;
+	  l = xtmp;
+	}
       while (r->parent != xr)
-	r->parent = xr;
-
+	{
+	  xtmp = r->parent;
+	  r->parent = xr;
+	  r = xtmp;
+	}
       *is_dead = xl->is_dead || xr->is_dead;
       tmp = tmp | xl->markset | xr->markset;
       if (xl->rank < xr->rank)
 	{
 	  xl->parent = r;
 	  xr->is_dead = *is_dead;
+	  xr->markset = tmp;
 	}
       else if (xr->rank < xl->rank)
 	{
 	  xr->parent = xl;
 	  xl->is_dead = *is_dead;
+	  xl->markset = tmp;
 	}
       else
 	{
 	  xl->parent = xr;
 	  xr->rank = xr->rank + 1;
 	  xr->is_dead = *is_dead;
+	  xr->markset = tmp;
 	}
 
       xr->mutex.unlock();
@@ -635,11 +508,61 @@ namespace spot
     }
 
   private:
-    uf_t* effective_uf;		///< \brief the union find
     hashtable_t *visited_;
     int thread_number_;
     std::atomic<int> size_;
     std::vector<node*> prealloc_;
   };
+
+
+
+
+
+
+
+
+
+
+    // inline bool try_insert(enum op_type op,
+    // 		    const fasttgba_state* left,
+    // 		    const fasttgba_state* right,
+    // 		    unsigned long acc,
+    // 		    int tn)
+    // {
+    //   shared_op* node = 0;
+    //   if(prealloc_idx_[tn] < prealloc_size_)
+    // 	{
+    // 	  node = &prealloc_[tn][prealloc_idx_[tn]];
+    // 	  ++prealloc_idx_[tn];
+    // 	}
+    //   else
+    // 	{
+    // 	  // End of the prealloc, use the last room to
+    // 	  // store pointer to a new prealloc range.
+    // 	  node = new shared_op[prealloc_size_+1];
+    // 	  prealloc_[tn][prealloc_size_].arg1_ = (void*) node;
+    // 	  prealloc_idx_[tn] = 1; // since node refer prealloc_[0]
+    // 	  prealloc_[tn] = node;
+    // 	  prealloc_[tn][prealloc_size_].arg1_ = 0;
+    // 	}
+    //   node->op_ = op;
+    //   node->arg1_ = (void*)left;
+    //   node->arg2_ = (void*)right;
+    //   node->acc_ = acc;
+    //   if (node->op_ == makedead)
+    // 	node->hash_ = left->hash() + acc;
+    //   else
+    // 	node->hash_ = left->hash() + right->hash() + acc;
+
+    //   map_key_t clone = 0;
+    //   if (ht_cas_empty (mem_table_, (map_key_t)node,
+    // 	  		(map_val_t)1, &clone, (void*)NULL)
+    // 	  == DOES_NOT_EXIST)
+    // 	return true;
+
+    //   // Insertion failed;
+    //   --prealloc_idx_[tn];
+    //   return false;
+    // }
 }
 #endif // SPOT_FASTTGBAALGOS_EC_CONCUR_UF_HH
