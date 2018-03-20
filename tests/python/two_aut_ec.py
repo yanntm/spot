94c39c1ad9f71377ebc6aedd596e4a70f7e0f186
Clément Gillard <clement.gillard@epita.fr>
2018-03-20 15:42:45 +0100

    two_aut_ec: Test accepting run search
    Add accepting run checks to the two_aut_ec checks.
    
    * tests/python/two_aut_ec.py: Here.

--- tests/python/two_aut_ec.py
+++ tests/python/two_aut_ec.py
@@ -19,16 +19,47 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import time
+import sys
 
 import spot
 
-g = spot.automata('randaut -A 4 -Q5 -n -1 --check=strength 2|')
+g = spot.automata('randaut -A "Inf(0)&Inf(1)|Inf(2)&Inf(3)" -Q5 -n -1'
+                  ' --check=strength 2|')
+
+count_ec = 0
+count_ar = 0
+
 end = time.time() + 10
+
 while time.time() < end:
-    a = spot.remove_fin(g.__next__())
-    b = spot.remove_fin(g.__next__())
-    if (spot.two_aut_ec(a, b) != spot.otf_product(a, b).is_empty()):
-        print("spot.two_aut_ec(a, b) != spot.otf_product(a, b).is_empty()")
-        print("\na:\n" + a.to_str())
-        print("\nb:\n" + b.to_str())
+    a = next(g)
+    b = next(g)
+    ra = spot.twa_run(a)
+    rb = spot.twa_run(b)
+
+    res = spot.two_aut_ec(a, b, ra, rb)
+
+    if spot.otf_product(a, b).is_empty() != res:
+        print("spot.two_aut_ec(a, b) != spot.otf_product(a, b).is_empty()",
+              file=sys.stderr)
+        print("\na:\n" + a.to_str(), file=sys.stderr)
+        print("\nb:\n" + b.to_str(), file=sys.stderr)
         exit(2)
+
+    count_ec += 1
+
+    if res == False:
+        if ra.replay(spot.get_cout()) == False:
+            print("Could not replay left accepting run", file=sys.stderr)
+            print("\na:\n" + a.to_str(), file=sys.stderr)
+            print("\nb:\n" + b.to_str(), file=sys.stderr)
+            exit(2)
+        if rb.replay(spot.get_cout()) == False:
+            print("Could not replay right accepting run", file=sys.stderr)
+            print("\na:\n" + a.to_str(), file=sys.stderr)
+            print("\nb:\n" + b.to_str(), file=sys.stderr)
+            exit(2)
+        count_ar += 1
+
+print("Computed {} emptiness checks and {} accepting runs".format(count_ec,
+                                                                  count_ar))
