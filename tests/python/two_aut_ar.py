c3fdb38adc0f3c2b7ddb529b4521365932021fcf
Clément Gillard <clement.gillard@epita.fr>
2018-04-09 10:41:52 +0200

    TOREMOVE test files for accepting runs

--- tests/python/two_aut_ar.py
+++ tests/python/two_aut_ar.py
@@ -18,20 +18,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
-
 import spot
 
-g = spot.automata('randaut -A 4 -Q5 -n -1 --check=strength 2|')
-a = spot.remove_fin(g.__next__())
-b = spot.remove_fin(g.__next__())
+def two_intersecting_automata(g):
+    """return two random automata with a non-empty intersection"""
+    for a, b in zip(g, g):
+        if a.intersects(b):
+            return a, b
 
-while spot.two_aut_ec(a, b):
-    a = spot.remove_fin(g.__next__())
-    b = spot.remove_fin(g.__next__())
+def two_non_intersecting_automata(g):
+    """return two random automata with an empty intersection"""
+    for a, b in zip(g, g):
+        if not a.intersects(b):
+            return a, b
 
-r = a.accepting_run()
+g = spot.automata('randaut -A "Inf(0)&Inf(1)|Inf(2)&Inf(3)" -Q5 -n -1'
+                  ' --check=strength 2|')
 
-print("before")
-spot.two_aut_ec(a, b, r)
-print("after")
+a, b = two_intersecting_automata(g)
+t = spot.two_aut_ec(a,b)
+print(a.intersects(b))
+print(bool(t))
+ar, br = t.accepting_runs()
+assert(ar.replay(spot.get_cout()))
+assert(br.replay(spot.get_cout()))
+
+a, b = two_non_intersecting_automata(g)
+t = spot.two_aut_ec(a,b)
+print(a.intersects(b))
+print(bool(t))
+assert(t is None)
