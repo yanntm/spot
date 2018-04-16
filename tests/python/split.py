0a20c9fda0e050cb7902b1a93d213172742fac6a
Maximilien Colange <colange@lrde.epita.fr>
2018-04-16 13:21:47 +0200

    test split

--- tests/python/split.py
+++ tests/python/split.py
@@ -19,11 +19,28 @@
 
 import spot
 
+def incl(a,b):
+    return not b.intersects(spot.dualize(spot.tgba_determinize(a)))
+def equiv(a,b):
+    return incl(a,b) and incl(b,a)
+
+def do_split(f, in_list):
+    aut = spot.translate(f)
+    inputs = spot.buddy.bddtrue
+    for a in in_list:
+        inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula(a)))
+    s = spot.split_automaton(aut, inputs)
+    return aut,s
+
+aut, s = do_split('(FG !a) <-> (GF b)', ['a'])
+assert equiv(aut, spot.unsplit(s))
+
 aut = spot.translate('GFa && GFb')
 inputs = spot.buddy.bddtrue
 inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula('a')))
 s = spot.split_automaton(aut, inputs)
 
+assert equiv(aut, spot.unsplit(s))
 assert s.to_str() == """HOA: v1
 States: 3
 Start: 0
@@ -50,6 +67,7 @@ inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula('go')))
 inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula('req')))
 s = spot.split_automaton(aut, inputs)
 
+assert equiv(aut, spot.unsplit(s))
 assert s.to_str() == """HOA: v1
 States: 9
 Start: 0
@@ -85,12 +103,118 @@ State: 8 {0}
 [!3] 2
 --END--"""
 
-
 aut = spot.translate('((G (((! g_0) || (! g_1)) && ((r_0 && (X r_1)) -> (F (g_0 && g_1))))) && (G (r_0 -> F g_0))) && (G (r_1 -> F g_1))')
 inputs = spot.buddy.bddtrue
 inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula('r_0')))
 inputs &= spot.buddy.bdd_ithvar(aut.get_dict().varnum(spot.formula('r_1')))
 s = spot.split_automaton(aut, inputs)
 
-print(s.to_str())
+assert equiv(aut, spot.unsplit(s))
+
+#sref = spot.automaton("""
+#HOA: v1
+#States: 28
+#Start: 0
+#AP: 4 "g_0" "g_1" "r_0" "r_1"
+#acc-name: generalized-Buchi 2
+#Acceptance: 2 Inf(0)&Inf(1)
+#properties: trans-labels explicit-labels trans-acc
+#--BODY--
+#State: 0
+#[2&3] 8
+#[!2&3] 9
+#[2&!3] 10
+#[!2&!3] 11
+#State: 8 "0, 2&3"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 9 "0, !2&3"
+#[!0&1] 0 {0 1}
+#[!1] 3 {0}
+#State: 10 "0, 2&!3"
+#[!1] 0 {0 1}
+#[!0] 1 {1}
+#[0&!1] 5 {0 1}
+#State: 11 "0, !2&!3"
+#[!0&1 | !1] 0 {0 1}
+#State: 1
+#[2&!3] 12
+#[!2&!3] 13
+#State: 12 "1, 2&!3"
+#[!0] 1 {1}
+#[0&!1] 5 {0 1}
+#State: 13 "1, !2&!3"
+#[0&!1] 0 {0 1}
+#[!0] 6 {1}
+#State: 2
+#[2&!3] 14
+#[!2&!3] 15
+#State: 14 "2, 2&!3"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 15 "2, !2&!3"
+#[0&!1] 3 {0}
+#[!0&1] 6 {1}
+#[!0&!1] 7
+#State: 3
+#[2] 16
+#[!2] 17
+#State: 16 "3, 2"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 17 "3, !2"
+#[!0&1] 0 {0 1}
+#[!1] 3 {0}
+#State: 4
+#[2&!3] 18
+#[!2&!3] 19
+#State: 18 "4, 2&!3"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 19 "4, !2&!3"
+#[!0&1] 0 {0 1}
+#[!1] 3 {0}
+#State: 5
+#[2&!3] 20
+#[!2&!3] 21
+#State: 20 "5, 2&!3"
+#[!0] 1 {1}
+#[0&!1] 5 {0 1}
+#State: 21 "5, !2&!3"
+#[!0 | !1] 0 {0 1}
+#State: 6
+#[2&3] 22
+#[!2&3] 23
+#[2&!3] 24
+#[!2&!3] 25
+#State: 22 "6, 2&3"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 23 "6, !2&3"
+#[0&!1] 3 {0}
+#[!0&1] 6 {1}
+#[!0&!1] 7
+#State: 24 "6, 2&!3"
+#[!0&1 | !0] 1 {1}
+#[0&!1] 5 {0 1}
+#State: 25 "6, !2&!3"
+#[0&!1] 0 {0 1}
+#[!0&1 | 0] 6 {1}
+#State: 7
+#[2] 26
+#[!2] 27
+#State: 26 "7, 2"
+#[!0&1] 1 {1}
+#[!0&!1] 2
+#[0&!1] 4 {0}
+#State: 27 "7, !2"
+#[0&!1] 3 {0}
+#[!0&1] 6 {1}
+#[!0&!1] 7
+#--END--""")
 
