e28c742875a611dbef2820b9cba30b60186a357f
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2018-04-26 19:31:44 +0200

    implement dealternation for very-weak and deterministic sub-part

--- spot/twaalgos/slaa2sdba.cc
+++ spot/twaalgos/slaa2sdba.cc
@@ -25,6 +25,7 @@
 #include <spot/twaalgos/sccinfo.hh>
 #include <spot/twaalgos/totgba.hh>
 #include <spot/twaalgos/isdet.hh>
+#include <spot/twaalgos/isweakscc.hh>
 #include <sstream>
 #include <tuple>
 
@@ -162,7 +163,8 @@ namespace spot
       // We use n BDD variables to represent the n states.
       // The state i is BDD variable state_base_+i.
       unsigned state_base_;
-      unsigned dot_base_;
+      std::vector<unsigned> dots_;
+      std::map<int, acc_cond::mark_t> var_to_mark_;
       bdd all_states_and_dots_;
       bool cutdet_;
       std::map<triplet_t, unsigned, triplet_lt> triplet_to_unsigned_;
@@ -170,6 +172,7 @@ namespace spot
       std::vector<slaa_to_sdba_state_type> types_;
       std::vector<bool> true_state_;
       bitvect_array* reachable_;
+      bitvect* det_from_now_;
       bdd stay_states;
     public:
       slaa_to_sdba_runner(const_twa_graph_ptr aut, bool cutdet)
@@ -191,15 +194,38 @@ namespace spot
               }
 
         reachable_ = make_bitvect_array(ns, ns);
-        scc_info si(aut, scc_info_options::TRACK_SUCCS);
+        det_from_now_ = make_bitvect(ns);
+        scc_info si(aut, scc_info_options::TRACK_SUCCS
+                    | scc_info_options::TRACK_STATES);
         unsigned nscc = si.scc_count();
         for (unsigned n = 0; n < nscc; ++n)
           {
             unsigned s = si.one_state_of(n);
             bitvect& svec = reachable_->at(s);
             svec.set(s);
+            bool isdet = true;
             for (unsigned d: si.succ(n))
-              svec |= reachable_->at(si.one_state_of(d));
+              {
+                unsigned dd = si.one_state_of(d);
+                svec |= reachable_->at(dd);
+                isdet &= det_from_now_->get(dd);
+              }
+            if (isdet && is_weak_scc(si, n))
+              {
+                bdd available = bddtrue;
+                for (auto&t : aut->out(s))
+                  if (!bdd_implies(t.cond, available))
+                    {
+                      isdet = false;
+                      break;
+                    }
+                  else
+                    {
+                      available -= t.cond;
+                    }
+                if (isdet)
+                  det_from_now_->set(s);
+              }
           }
       }
 
@@ -207,6 +233,7 @@ namespace spot
       {
         aut_->get_dict()->unregister_all_my_variables(this);
         delete reachable_;
+        delete det_from_now_;
       }
 
       bdd state_bdd(unsigned num)
@@ -216,7 +243,7 @@ namespace spot
 
       bdd dot_bdd(unsigned num)
       {
-        return bdd_ithvar(dot_base_ + num);
+        return bdd_ithvar(dots_[num]);
       }
 
       unsigned state_num(bdd b)
@@ -284,12 +311,12 @@ namespace spot
               {
                 assert(bdd_low(cube) == bddfalse);
                 unsigned v = bdd_var(cube);
-                if (!inner_first)
-                  out << "·";
-                if ((dot_base_ <= v) && (v < dot_base_ + ns))
-                  out << '(' << (v - dot_base_) << ')';
-                else
-                  out << state_num(cube);
+                if ((state_base_ <= v) && (v < state_base_ + ns))
+                  {
+                    if (!inner_first)
+                      out << "·";
+                    out << state_num(cube);
+                  }
                 cube = bdd_high(cube);
                 inner_first = false;
               }
@@ -313,15 +340,10 @@ namespace spot
       {
         auto d = aut_->get_dict();
 
-        twa_graph_ptr res = make_twa_graph(d);
-        res->copy_ap_of(aut_);
-        res->prop_copy(aut_, {false, false, false, false, false, true});
-        res->set_buchi();
-
         // We need one BDD variable per possible output state.  If
         unsigned ns = aut_->num_states();
         state_base_ = d->register_anonymous_variables(ns, this);
-        dot_base_ = d->register_anonymous_variables(ns, this);
+        dots_.resize(ns);
         all_states_and_dots_ = bddtrue;
         bdd all_dots = bddtrue;
         bdd leave_states = bddtrue;
@@ -330,13 +352,13 @@ namespace spot
         bdd must_stay_states = bddtrue;
         bitvect* stay_bitvect = make_bitvect(ns);
         bitvect* tmp_bitvect = make_bitvect(ns);
+        unsigned mark_pos = 0;
+        acc_cond::mark_t all_marks = {};
 
         for (unsigned s = 0; s < ns; ++s)
           {
             bdd sb = state_bdd(s);
-            bdd db = dot_bdd(s);
-            all_states_and_dots_ &= sb & db;
-            all_dots &= db;
+            all_states_and_dots_ &= sb;
             if (types_[s] == State_Leave)
               {
                 leave_states &= sb;
@@ -350,13 +372,39 @@ namespace spot
                 else
                   must_stay_states &= sb;
               }
+
+            // If this state has a rejecting loop, we will need some
+            // mark.
+            for (auto& e: aut_->out(s))
+              {
+                if (!e.acc)
+                  continue;
+                for (unsigned dst: aut_->univ_dests(e))
+                  if (dst == s)
+                    {
+                      int v = d->register_anonymous_variables(1, this);
+                      dots_[s] = v;
+                      all_marks.set(mark_pos);
+                      var_to_mark_.emplace(v, acc_cond::mark_t({mark_pos++}));
+                      all_dots &= dot_bdd(s);
+                      goto needmark_done;
+                    }
+              }
+          needmark_done:
+            ;
           }
+        all_states_and_dots_ &= all_dots;
         if (cutdet_)
           {
             leave_states &= all_dots;
             must_stay_states &= all_dots;
             may_stay_states &= all_dots;
           }
+
+        twa_graph_ptr res = make_twa_graph(d);
+        res->copy_ap_of(aut_);
+        res->prop_copy(aut_, {false, false, false, false, false, true});
+        res->set_generalized_buchi(mark_pos);
 
         // Compute a BDD representing the successors of one
         // state-formula.  The successor representation is a
@@ -373,13 +421,11 @@ namespace spot
                   {
                     assert(bdd_low(cube) == bddfalse);
                     unsigned v = bdd_var(cube);
-                    if ((dot_base_ <= v) && (v < dot_base_ + ns))
+                    if ((state_base_ <= v) && (v < state_base_ + ns))
                       {
-                        cube = bdd_high(cube);
-                        continue;
+                        unsigned num = state_num(cube);
+                        inner_res &= state_comp_as_bdd(num, component);
                       }
-                    unsigned num = state_num(cube);
-                    inner_res &= state_comp_as_bdd(num, component);
                     cube = bdd_high(cube);
                   }
                 res |= inner_res;
@@ -410,6 +456,7 @@ namespace spot
           init &= state_bdd(d);
         new_state(triplet_t{init, bddfalse, bddtrue});
 
+      next_state:
         while (!todo.empty())
           {
             unsigned src_state = todo.back();
@@ -418,12 +465,11 @@ namespace spot
             bdd src_left, src_right, src_comp;
             std::tie(src_left, src_right, src_comp) = src;
 
-
             if (src_comp == bddtrue)
               {
                 if (src_left == bddtrue)
                   {
-                    res->new_edge(src_state, src_state, bddtrue, {0});
+                    res->new_edge(src_state, src_state, bddtrue, all_marks);
                     continue;
                   }
                 else if (bdd_restrict(src_left, stay_states) != src_left)
@@ -432,6 +478,52 @@ namespace spot
                     bdd cube;
                     while ((cube = isop.next()) != bddfalse)
                       {
+                        // If all the states of cube can only reach
+                        // deterministic part, we want to do the
+                        // standard dealternation construction.
+                        bdd tmp = cube;
+                        bool alldet = true;
+                        while (tmp != bddtrue)
+                          {
+                            unsigned num = state_num(tmp);
+                            if (!det_from_now_->get(num))
+                              {
+                                alldet = false;
+                                break;
+                              }
+                            tmp = bdd_high(tmp);
+                          }
+                        if (alldet)
+                          {
+                            bdd dests = delta(cube, bddtrue);
+                            bdd aps = bdd_exist(bdd_support(dests),
+                                                all_states_and_dots_);
+                            bdd all = bddtrue;
+                            while (all != bddfalse)
+                              {
+                                bdd letter = bdd_satoneset(all, aps, bddfalse);
+                                all -= letter;
+                                bdd trans = bdd_restrict(dests, letter);
+                                if (trans == bddfalse)
+                                  continue;
+                                bdd dest = bdd_exist(trans, all_dots);
+                                bdd dots = bdd_existcomp(trans, all_dots);
+                                acc_cond::mark_t m = all_marks;
+                                while (dots != bddtrue)
+                                  {
+                                    int v = bdd_var(dots);
+                                    m -= var_to_mark_[v];
+                                    dots = bdd_high(dots);
+                                  }
+                                res->new_edge(src_state,
+                                              new_state(triplet_t{dest,
+                                                    bddfalse, bddtrue}),
+                                              letter, m);
+
+                              }
+                            goto next_state;
+                          }
+
                         bdd local_stay_states = bdd_exist(cube, leave_states);
                         if (local_stay_states == bddfalse)
                           continue;
@@ -527,7 +619,7 @@ namespace spot
                         dst = new_state(triplet_t
                                         {bdd_restrict(right, src_comp),
                                          src_comp, src_comp});
-                        m = {0};
+                        m = all_marks;
                       }
                     else
                       {
