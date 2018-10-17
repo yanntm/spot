8f138055e5bb30987b1b9600b2b5ea624464830a
Etienne Renault <renault@lrde.epita.fr>
2018-10-17 16:05:17 +0200

    laarman: implement safety and liveness
    * spot/mc/Makefile.am,
    spot/mc/cond_dest.hh,
    spot/mc/laarman_safety.hh,
    spot/mc/laarman_source.hh,
    spot/mc/mc.hh,
    tests/ltsmin/modelcheck.cc: here.

--- tests/ltsmin/modelcheck.cc
+++ tests/ltsmin/modelcheck.cc
@@ -66,6 +66,8 @@ const unsigned RENAULT_COND_SOURCE = 260;
 const unsigned RENAULT_SAFETY = 261;
 const unsigned NONE = 262;
 const unsigned BLOEMEN_SAFETY = 263;
+const unsigned LAARMAN_SOURCE = 264;
+const unsigned LAARMAN_SAFETY = 265;
 
 // Handle all options specified in the command line
 struct mc_options_
@@ -90,6 +92,8 @@ struct mc_options_
   bool renault_cond_dest = false;
   bool renault_cond_source = false;
   bool renault_safety = false;
+  bool laarman_source = false;
+  bool laarman_safety = false;
   bool none = false;
   bool use_por = false;
 } mc_options;
@@ -127,6 +131,12 @@ parse_opt_finput(int key, char* arg, struct argp_state*)
       break;
     case NONE:
       mc_options.none = true;
+      break;
+    case LAARMAN_SOURCE:
+      mc_options.laarman_source = true;
+      break;
+    case LAARMAN_SAFETY:
+      mc_options.laarman_safety = true;
       break;
     case COUNTEREXAMPLE:
       mc_options.compute_counterexample = true;
@@ -208,6 +218,10 @@ static const argp_option options[] =
       "run the a swarmed renault_safety POR algorithm (with --POR)", 0 },
     { "none", NONE, nullptr, 0,
       "explore the reduced graph (with --POR)", 0 },
+    { "laarman_source", COND_SOURCE, nullptr, 0,
+      "run the blue walk of CNDFS POR algorithm (with --POR)", 0 },
+    { "laarman_safety", COND_SOURCE, nullptr, 0,
+      "run the safety exploration proposed by Laarman et al. (with --POR)", 0 },
     { "counterexample", COUNTEREXAMPLE, nullptr, 0,
       "compute an accepting counterexample (if it exists)", 0 },
     { "is-empty", 'e', nullptr, 0,
@@ -1049,6 +1063,198 @@ static int checked_main()
                       << split_filename(mc_options.model)
                       << ','
                       << tm.timer("cond_source").walltime() << ','
+                      << memused << ','
+                      << inserted << ','
+                      << st << ','
+                      << tr << ','
+                      << sccs
+                      << '\n';
+          }
+      }
+
+    if (mc_options.laarman_source &&  mc_options.model != nullptr)
+      {
+        unsigned int hc = std::thread::hardware_concurrency();
+        if (mc_options.nb_threads > hc)
+          std::cerr << "Warning: you require " << mc_options.nb_threads
+                    << " threads, but your computer only support " << hc
+                    << ". This could slow down parallel algorithms.\n";
+
+        tm.start("load kripkecube");
+        spot::ltsmin_kripkecube_ptr modelcube = nullptr;
+        try
+          {
+            modelcube = spot::ltsmin_model::load(mc_options.model)
+              .kripkecube({}, deadf, mc_options.compress,
+                          mc_options.nb_threads, mc_options.use_por);
+          }
+        catch (const std::runtime_error& e)
+          {
+            std::cerr << e.what() << '\n';
+          }
+        tm.stop("load kripkecube");
+
+        int memused = spot::memusage();
+        tm.start("laarman_source");
+        auto res = spot::laarman_source<spot::ltsmin_kripkecube_ptr,
+                                     spot::cspins_state,
+                                     spot::cspins_iterator,
+                                     spot::cspins_state_hash,
+                                     spot::cspins_state_equal>(modelcube);
+        tm.stop("laarman_source");
+        memused = spot::memusage() - memused;
+
+        if (!modelcube)
+          {
+            exit_code = 2;
+            goto safe_exit;
+          }
+
+        // Display statistics
+        unsigned sccs = 0;
+        unsigned st = 0;
+        unsigned tr = 0;
+        unsigned inserted = 0;
+        for (unsigned i = 0; i < res.first.size(); ++i)
+          {
+            std::cout << "\n---- Thread number : " << i << '\n';
+            std::cout << res.first[i].states << " unique states visited\n";
+            std::cout << res.first[i].inserted << " unique states inserted\n";
+            std::cout << res.first[i].transitions
+                      << " transitions explored\n";
+            std::cout << res.first[i].sccs << " sccs found\n";
+            std::cout << res.first[i].walltime
+                      << " milliseconds\n";
+
+            sccs += res.first[i].sccs;
+            st += res.first[i].states;
+            tr += res.first[i].transitions;
+            inserted += res.first[i].inserted;
+
+            if (mc_options.csv)
+              {
+                std::cout << "Find following the csv: "
+                          << "thread_id,walltimems,"
+                          << "states,transitions,sccs\n";
+                std::cout << "@th_" << i << ','
+                          << res.first[i].walltime << ','
+                          << res.first[i].states << ','
+                          << res.first[i].inserted << ','
+                          << res.first[i].transitions << ','
+                          << res.first[i].sccs
+                          << std::endl;
+              }
+          }
+
+        if (mc_options.csv)
+          {
+            std::cout << "\nSummary :\n";
+            std::cout << "Find following the csv: "
+                      << "model,walltimems,memused,"
+                      << "inserted_states,"
+                      << "cumulated_states,cumulated_transitions,"
+                      << "cumulated_sccs\n";
+
+            std::cout << '#'
+                      << split_filename(mc_options.model)
+                      << ','
+                      << tm.timer("laarman_source").walltime() << ','
+                      << memused << ','
+                      << inserted << ','
+                      << st << ','
+                      << tr << ','
+                      << sccs
+                      << '\n';
+          }
+      }
+
+    if (mc_options.laarman_safety &&  mc_options.model != nullptr)
+      {
+        unsigned int hc = std::thread::hardware_concurrency();
+        if (mc_options.nb_threads > hc)
+          std::cerr << "Warning: you require " << mc_options.nb_threads
+                    << " threads, but your computer only support " << hc
+                    << ". This could slow down parallel algorithms.\n";
+
+        tm.start("load kripkecube");
+        spot::ltsmin_kripkecube_ptr modelcube = nullptr;
+        try
+          {
+            modelcube = spot::ltsmin_model::load(mc_options.model)
+              .kripkecube({}, deadf, mc_options.compress,
+                          mc_options.nb_threads, mc_options.use_por);
+          }
+        catch (const std::runtime_error& e)
+          {
+            std::cerr << e.what() << '\n';
+          }
+        tm.stop("load kripkecube");
+
+        int memused = spot::memusage();
+        tm.start("laarman_safety");
+        auto res = spot::laarman_safety<spot::ltsmin_kripkecube_ptr,
+                                     spot::cspins_state,
+                                     spot::cspins_iterator,
+                                     spot::cspins_state_hash,
+                                     spot::cspins_state_equal>(modelcube);
+        tm.stop("laarman_safety");
+        memused = spot::memusage() - memused;
+
+        if (!modelcube)
+          {
+            exit_code = 2;
+            goto safe_exit;
+          }
+
+        // Display statistics
+        unsigned sccs = 0;
+        unsigned st = 0;
+        unsigned tr = 0;
+        unsigned inserted = 0;
+        for (unsigned i = 0; i < res.first.size(); ++i)
+          {
+            std::cout << "\n---- Thread number : " << i << '\n';
+            std::cout << res.first[i].states << " unique states visited\n";
+            std::cout << res.first[i].inserted << " unique states inserted\n";
+            std::cout << res.first[i].transitions
+                      << " transitions explored\n";
+            std::cout << res.first[i].sccs << " sccs found\n";
+            std::cout << res.first[i].walltime
+                      << " milliseconds\n";
+
+            sccs += res.first[i].sccs;
+            st += res.first[i].states;
+            tr += res.first[i].transitions;
+            inserted += res.first[i].inserted;
+
+            if (mc_options.csv)
+              {
+                std::cout << "Find following the csv: "
+                          << "thread_id,walltimems,"
+                          << "states,transitions,sccs\n";
+                std::cout << "@th_" << i << ','
+                          << res.first[i].walltime << ','
+                          << res.first[i].states << ','
+                          << res.first[i].inserted << ','
+                          << res.first[i].transitions << ','
+                          << res.first[i].sccs
+                          << std::endl;
+              }
+          }
+
+        if (mc_options.csv)
+          {
+            std::cout << "\nSummary :\n";
+            std::cout << "Find following the csv: "
+                      << "model,walltimems,memused,"
+                      << "inserted_states,"
+                      << "cumulated_states,cumulated_transitions,"
+                      << "cumulated_sccs\n";
+
+            std::cout << '#'
+                      << split_filename(mc_options.model)
+                      << ','
+                      << tm.timer("laarman_safety").walltime() << ','
                       << memused << ','
                       << inserted << ','
                       << st << ','
