5c3a33f72057ce4b7247138b9bcd4a4f938e8e11
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2019-05-24 23:26:49 +0200

    test: add missing copyright boilerplate
    * tests/python/genem.py: Here.

--- tests/python/genem.py
+++ tests/python/genem.py
@@ -1,3 +1,26 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (C) 2018-2019 Laboratoire de Recherche et Développement de l'Epita
+# (LRDE).
+#
+# This file is part of Spot, a model checking library.
+#
+# Spot is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spot is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Test that the spot.gen package works, in particular, we want
+# to make sure that the objects created from spot.gen methods
+# are usable with methods from the spot package.
+
 import spot
 
 a1 = spot.automaton('''
