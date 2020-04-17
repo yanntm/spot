68012e6a8571f2824a94a901968cb5453e6f4d37
Florian Renkin <frenkin@lrde.epita.fr>
2020-04-17 16:47:02 +0200

    to_parity: Correct the expected number of states
    * tests/python/toparity.py: here.

--- tests/python/toparity.py
+++ tests/python/toparity.py
@@ -195,7 +195,7 @@ State: 13
 [0&1] 5
 [!0&!1] 10 {0 1 3 5}
 [0&!1] 13 {1 3}
---END--"""), [35, 30, 23, 30, 34, 27, 22])
+--END--"""), [35, 28, 23, 30, 29, 25, 22])
 
 test(spot.automaton("""
 HOA: v1
@@ -213,7 +213,7 @@ State: 1
 [0&!1] 1 {4}
 [!0&1] 1 {0 1 2 3}
 [!0&!1] 1 {0 3}
---END--"""), [7, 7, 3, 6, 10, 5, 3])
+--END--"""), [7, 5, 3, 6, 5, 5, 3])
 
 
 for i,f in enumerate(spot.randltl(10, 400)):
@@ -288,7 +288,7 @@ State: 4
 [0&!1] 4
 [0&1] 4 {1 2 4}
 --END--
-"""), [9, 9, 8, 7, 9, 9, 6])
+"""), [9, 6, 7, 7, 6, 6, 6])
 
 test(spot.automaton("""
 HOA: v1
@@ -310,7 +310,7 @@ State: 1
 [0&!1] 1 {2 3}
 [0&1] 1 {1 2 4}
 --END--
-"""), [11, 6, 4, 7, 11, 6, 3])
+"""), [11, 6, 3, 7, 7, 4, 3])
 
 
 # Tests both the old and new version of to_parity
@@ -341,7 +341,7 @@ explicit-labels trans-acc --BODY-- State: 0 [0&1] 2 {4 5} [0&1] 4 {0 4}
 p = spot.to_parity_old(a, True)
 assert p.num_states() == 22
 assert spot.are_equivalent(a, p)
-test(a, [8, 7, 8, 8, 8, 7, 6])
+test(a, [8, 7, 8, 8, 6, 7, 6])
 
 # Force a few edges to false, to make sure to_parity() is OK with that.
 for e in a.out(2):
@@ -355,7 +355,7 @@ for e in a.out(3):
 p = spot.to_parity_old(a, True)
 assert p.num_states() == 22
 assert spot.are_equivalent(a, p)
-test(a, [7, 6, 7, 7, 7, 6, 6])
+test(a, [7, 6, 7, 7, 6, 6, 6])
 
 for f in spot.randltl(4, 400):
     d = spot.translate(f, "det", "G")
