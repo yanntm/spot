2ddb742ca99cb92dbbda7180829aa09dd84592cf
Etienne Renault <renault@lrde.epita.fr>
2020-05-14 17:22:22 +0200

    sanity: please 80 columns
    * spot/ltsmin/spins_kripke.hh,
    spot/twacube/twacube.cc,
    spot/twacube_algos/convert.cc: Here.

--- spot/ltsmin/spins_kripke.hh
+++ spot/ltsmin/spins_kripke.hh
@@ -1,6 +1,6 @@
 // -*- coding: utf-8 -*-
-// Copyright (C) 2017, 2018, 2019, 2020 Laboratoire de Recherche et Développement de
-// l'Epita (LRDE)
+// Copyright (C) 2017, 2018, 2019, 2020 Laboratoire de Recherche et
+// Développement de l'Epita (LRDE)
 //
 // This file is part of Spot, a model checking library.
 //
--- spot/twacube_algos/convert.cc
+++ spot/twacube_algos/convert.cc
@@ -1,6 +1,6 @@
 // -*- coding: utf-8 -*-
-// Copyright (C) 2015, 2016, 2018, 2020 Laboratoire de Recherche et Developpement de
-// l'Epita (LRDE).
+// Copyright (C) 2015, 2016, 2018, 2020 Laboratoire de Recherche et
+// Developpement de l'Epita (LRDE).
 //
 // This file is part of Spot, a model checking library.
 //
