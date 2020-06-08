77d18c5c3e89380d7edec23bd7a909baa3ac9c6b
Thibault Allançon <haltode@gmail.com>
2020-06-08 16:26:22 +0200

    mc: reset deadlock_bitstate.hh implementation
    Redo implementation to match deadlock.hh code.
    
    * spot/mc/deadlock_bitstate.hh: update here.

--- spot/mc/deadlock_bitstate.hh
+++ spot/mc/deadlock_bitstate.hh
@@ -41,55 +41,72 @@ namespace spot
            typename StateHash, typename StateEqual>
   class swarmed_deadlock_bitstate
   {
-    struct deadlock_state
-    {
-      State st;
-
-      deadlock_state(const State& initial)
-        : st(initial)
-      { }
-
-      deadlock_state() = default;
-
-      bool operator==(const deadlock_state& other) const
+    /// \brief Describes the status of a state
+    enum st_status
       {
-        StateEqual equal;
-        return equal(st, other.st);
-      }
+        UNKNOWN = 1,    // First time this state is discoverd by this thread
+        OPEN = 2,       // The state is currently processed by this thread
+        CLOSED = 4,     // All the successors of this state have been visited
+      };
+
+    /// \brief Describes the structure of a shared state
+    struct deadlock_pair
+    {
+      State st;                 ///< \brief the effective state
+      int* colors;              ///< \brief the colors (one per thread)
     };
 
-    /// \brief Internal state hasher for the Brick interface
-    struct brick_state_hasher : brq::hash_adaptor<deadlock_state>
+    /// \brief The hasher for the previous state.
+    struct pair_hasher : brq::hash_adaptor<deadlock_pair*>
     {
-      brick_state_hasher(const deadlock_state&)
+      pair_hasher(const deadlock_pair*)
       { }
 
-      brick_state_hasher() = default;
+      pair_hasher() = default;
 
-      auto hash(const deadlock_state& lhs) const
+      auto hash(const deadlock_pair* lhs) const
       {
         StateHash hash;
         // Not modulo 31 according to brick::hashset specifications.
-        unsigned u = hash(lhs.st) % (1<<30);
+        unsigned u = hash(lhs->st) % (1<<30);
         return u;
+      }
+
+      bool equal(const deadlock_pair* lhs,
+                 const deadlock_pair* rhs) const
+      {
+        StateEqual equal;
+        return equal(lhs->st, rhs->st);
+      }
+
+      // WARNING: temporary technical fix to have pointers in brick hash table
+      using hash64_t = uint64_t;
+      template<typename cell>
+      typename cell::pointer match(cell &c, const deadlock_pair* t,
+                                   hash64_t h) const
+      {
+        // NOT very sure that dereferencing will not kill some brick property
+        return c.match(h) && equal(c.fetch() , t) ? c.value() : nullptr;
       }
     };
 
   public:
 
     ///< \brief Shortcut to ease shared map manipulation
-    using shared_map = brq::concurrent_hash_set<deadlock_state>;
+    using shared_map = brq::concurrent_hash_set<deadlock_pair*>;
 
     swarmed_deadlock_bitstate(kripkecube<State, SuccIterator>& sys,
-                              shared_map& map, size_t mem_size, unsigned tid,
-                              std::atomic<bool>& stop):
+                              shared_map& map, size_t mem_size,
+                              unsigned tid, std::atomic<bool>& stop):
       sys_(sys), tid_(tid), map_(map),
+      bloom_filter_(mem_size),
       nb_th_(std::thread::hardware_concurrency()),
-      stop_(stop),
-      bloom_filter_(mem_size)
+      p_(sizeof(int)*std::thread::hardware_concurrency()),
+      p_pair_(sizeof(deadlock_pair)),
+      stop_(stop)
     {
       static_assert(spot::is_a_kripkecube_ptr<decltype(&sys),
-                                              State, SuccIterator>::value,
+                                             State, SuccIterator>::value,
                     "error: does not match the kripkecube requirements");
     }
 
@@ -107,32 +124,53 @@ namespace spot
       tm_.start("DFS thread " + std::to_string(tid_));
     }
 
-    bool push(deadlock_state s)
+    bool push(State s)
     {
-      auto it = map_.insert(s.st, brick_state_hasher());
-      if (!it.isnew())
+      // Prepare data for a newer allocation
+      int* ref = (int*) p_.allocate();
+      for (unsigned i = 0; i < nb_th_; ++i)
+        ref[i] = UNKNOWN;
+
+      // Try to insert the new state in the shared map.
+      deadlock_pair* v = (deadlock_pair*) p_pair_.allocate();
+      v->st = s;
+      v->colors = ref;
+      auto it = map_.insert(v, pair_hasher());
+      bool b = it.isnew();
+
+      // Insertion failed, delete element
+      // FIXME Should we add a local cache to avoid useless allocations?
+      if (!b)
+        p_.deallocate(ref);
+
+      // The state has been mark dead by another thread
+      for (unsigned i = 0; !b && i < nb_th_; ++i)
+        if ((*it)->colors[i] == static_cast<int>(CLOSED))
+          return false;
+
+      // The state has already been visited by the current thread
+      if ((*it)->colors[tid_] == static_cast<int>(OPEN))
         return false;
-      if (bloom_filter_.contains(state_hash_(s.st)))
-        return false;
+
+      // Keep a ptr over the array of colors
+      refs_.push_back((*it)->colors);
+
+      // Mark state as visited.
+      (*it)->colors[tid_] = OPEN;
       ++states_;
       return true;
     }
 
-    void pop()
+    bool pop()
     {
       // Track maximum dfs size
       dfs_ = todo_.size()  > dfs_ ? todo_.size() : dfs_;
 
-      auto elem = todo_.back();
-
-      deadlock_ = elem.current_tr == transitions_;
-      if (SPOT_UNLIKELY(deadlock_))
-        return;
-
-      todo_.pop_back();
-      sys_.recycle(elem.it, tid_);
-      map_.erase(elem.st, brick_state_hasher());
-      bloom_filter_.insert(state_hash_(elem.st.st));
+      // Don't avoid pop but modify the status of the state
+      // during backtrack
+      refs_.back()[tid_] = CLOSED;
+      refs_.pop_back();
+      return true;
     }
 
     void finalize()
@@ -154,36 +192,40 @@ namespace spot
     void run()
     {
       setup();
-      deadlock_state initial{sys_.initial(tid_)};
+      State initial = sys_.initial(tid_);
       if (SPOT_LIKELY(push(initial)))
-      {
-        todo_.push_back({initial, sys_.succ(initial.st, tid_), transitions_});
-      }
+        {
+          todo_.push_back({initial, sys_.succ(initial, tid_), transitions_});
+        }
       while (!todo_.empty() && !stop_.load(std::memory_order_relaxed))
-      {
-        if (todo_.back().it->done())
         {
-          pop();
-          if (deadlock_)
-            break;
-        }
-        else
-        {
-          ++transitions_;
-          State dst = todo_.back().it->state();
-          deadlock_state next{dst};
-
-          if (SPOT_LIKELY(push(next)))
-          {
-            todo_.back().it->next();
-            todo_.push_back({next, sys_.succ(dst, tid_), transitions_});
-          }
+          if (todo_.back().it->done())
+            {
+              if (SPOT_LIKELY(pop()))
+                {
+                  deadlock_ = todo_.back().current_tr == transitions_;
+                  if (deadlock_)
+                    break;
+                  sys_.recycle(todo_.back().it, tid_);
+                  todo_.pop_back();
+                }
+            }
           else
-          {
-            todo_.back().it->next();
-          }
+            {
+              ++transitions_;
+              State dst = todo_.back().it->state();
+
+              if (SPOT_LIKELY(push(dst)))
+                {
+                  todo_.back().it->next();
+                  todo_.push_back({dst, sys_.succ(dst, tid_), transitions_});
+                }
+              else
+                {
+                  todo_.back().it->next();
+                }
+            }
         }
-      }
       finalize();
     }
 
@@ -205,24 +247,28 @@ namespace spot
   private:
     struct todo__element
     {
-      deadlock_state st;
+      State s;
       SuccIterator* it;
       unsigned current_tr;
     };
-
-    kripkecube<State, SuccIterator>& sys_; ///< \brief The system to check
-    std::vector<todo__element> todo_;      ///< \brief The DFS stack
-    unsigned transitions_ = 0;             ///< \brief Number of transitions
-    unsigned tid_;                         ///< \brief Thread's current ID
-    shared_map map_;                       ///< \brief Map shared by threads
-    StateHash state_hash_;                 ///< \brief Spot state hasher
-    concurrent_bloom_filter bloom_filter_; ///< \brief Spot Bloom filter
-    spot::timer_map tm_;                   ///< \brief Time execution
-    unsigned states_ = 0;                  ///< \brief Number of states
-    unsigned dfs_ = 0;                     ///< \brief Maximum DFS stack size
+    kripkecube<State, SuccIterator>& sys_;  ///< \brief The system to check
+    std::vector<todo__element> todo_;       ///< \brief The DFS stack
+    unsigned transitions_ = 0;              ///< \brief Number of transitions
+    unsigned tid_;                          ///< \brief Thread's current ID
+    shared_map map_;                        ///< \brief Map shared by threads
+    StateHash state_hash_;                  ///< \brief Spot state hasher
+    concurrent_bloom_filter bloom_filter_;  ///< \brief Spot Bloom filter
+    spot::timer_map tm_;                    ///< \brief Time execution
+    unsigned states_ = 0;                   ///< \brief Number of states
+    unsigned dfs_ = 0;                      ///< \brief Maximum DFS stack size
     /// \brief Maximum number of threads that can be handled by this algorithm
     unsigned nb_th_ = 0;
-    bool deadlock_ = false;                ///< \brief Deadlock detected?
-    std::atomic<bool>& stop_;              ///< \brief Stop-the-world boolean
+    fixed_size_pool<pool_type::Unsafe> p_;  ///< \brief Color Allocator
+    fixed_size_pool<pool_type::Unsafe> p_pair_;  ///< \brief State Allocator
+    bool deadlock_ = false;                 ///< \brief Deadlock detected?
+    std::atomic<bool>& stop_;               ///< \brief Stop-the-world boolean
+    /// \brief Stack that grows according to the todo stack. It avoid multiple
+    /// concurent access to the shared map.
+    std::vector<int*> refs_;
   };
 }
