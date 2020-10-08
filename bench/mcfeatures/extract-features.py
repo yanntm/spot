d3afbf0443f640b16ed67db11aedaecd77569edf
Etienne Benrey <ebenrey@lrde.epita.fr>
2020-10-08 14:42:29 +0200

    mcfeatures: add options parser to feature extraction
    * extract-features.py: Here.

--- bench/mcfeatures/extract-features.py
+++ bench/mcfeatures/extract-features.py
@@ -18,37 +18,51 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from optparse import OptionParser
 import csv
 import os
 import subprocess
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit(1)
-infile = sys.argv[1]
-outfile = sys.argv[2]
+def get_options():
+    parser = OptionParser(usage="usage: %prog [options] filename",
+                          version='%prog')
 
-with open(outfile, 'w+') as f:
-    f.write('model,formula,time,memory,emptiness,states,transitions,'
-            + 'bloemen_time,cndfs_time,'
-            + 'incidence_in,incidence_out,average_incidence_ratio,'
-            + 'repeated_transitions,terminal,weak,inherently_weak,'
-            + 'very_weak,complete,universal,unambiguous,'
-            + 'semi_deterministic,stutter_invariant,sccs\n')
+    parser.add_option('-v', '--verbose', action='store_true')
+    parser.add_option('-t', '--time-only', action='store_true', dest='timeonly')
+    parser.add_option('-p', '--threads', action='append')
+    parser.add_option('-o', '--out', action='store', dest='fileout',
+                      default=None)
+    parser.add_option('--exe', action='store',
+                      default='../../tests/ltsmin/modelcheck')
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+    return options, args[0]
 
-with open(infile, newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        model = row[0]
-        formula = row[1]
-        row[0] = os.path.basename(model)
-        print(row[0])
+def get_fields(options):
+    fields = ['model', 'formula', 'time', 'memory', 'emptiness', 'states',
+              'transitions']
+    if not options.timeonly:
+        fields += ['sccs', 'repeated_transitions']
+        fields += ['incidence_in', 'incidence_out', 'average_incidence_ratio',
+                   'repeated_transitions', 'terminal', 'weak',
+                   'inherently_weak', 'very_weak', 'complete', 'universal',
+                   'unambiguous', 'semi_deterministic', 'stutter_invariant']
+    if not options.threads:
+        fields += ['bloemen_time', 'cndfs_time']
+    else:
+        for nb_threads in options.threads:
+            fields += ['bloemen_time_' + nb_threads, 'cndfs_time_' + nb_threads]
+    return fields
 
-        csvline = ','.join(row)
 
-        # bloemen time
-        p = subprocess.Popen(['../../tests/ltsmin/modelcheck', '-B', '-t',
-                              '-m', model, '-f', formula],
+def extract(options, filein, fields):
+    def get_time(option, threads, row):
+        p = subprocess.Popen([options.exe, option, '-t',
+                              '-m', row['model'], '-f', row['formula'],
+                              '-p', str(threads)],
                               stdout=subprocess.PIPE)
         p.wait()
         output = p.stdout.readlines()
@@ -56,40 +70,71 @@ with open(infile, newline='') as csvfile:
         for line in output:
             line = line.decode()
             if 'TOTAL' in line:
-                csvline += ',' + line.split()[-3]
+                return line.split()[-3]
 
-        # cndfs time
-        p = subprocess.Popen(['../../tests/ltsmin/modelcheck', '-C', '-t',
-                              '-m', model, '-f', formula],
-                              stdout=subprocess.PIPE)
-        p.wait()
-        output = p.stdout.readlines()
-        del p
-        for line in output:
-            line = line.decode()
-            if 'TOTAL' in line:
-                csvline += ',' + line.split()[-3]
+    with open(filein, newline='') as csvin:
+        with open(options.fileout, 'w', newline='') as csvout:
+            reader = csv.reader(csvin)
+            writer = csv.DictWriter(csvout, fieldnames=fields)
+            writer.writeheader()
 
+            for row in reader:
+                print(os.path.basename(row[0]))
+                rowdict = {}
+                for i in range(7):
+                    rowdict[fields[i]] = row[i]
 
-        # other features
-        p = subprocess.Popen(['../../tests/ltsmin/modelcheck', '-x',
-                              '-m', model, '-f', formula],
-                              stdout=subprocess.PIPE)
-        p.wait()
-        output = p.stdout.readlines()
-        del p
-        for i in range(len(output)):
-            line = str(output[i])
-            if 'incidence' in line:
-                csvline = csvline + ',' + output[i + 1].decode()[:-1]
-            if 'terminal' in line:
-                csvline = csvline + ',' + output[i + 1].decode()[:-1]
-            if 'sccs' in line:
-                csvline = csvline + ',' + output[i + 1].decode().split(',')[-3]
-                break
+                if not options.threads:
+                    rowdict['bloemen_time'] = get_time('-B', 1, rowdict)
+                    rowdict['cndfs_time'] = get_time('-C', 1, rowdict)
+                else:
+                    for nbt in options.threads:
+                        rowdict['bloemen_time_' + nbt] =\
+                                                get_time('-B', nbt, rowdict)
+                        rowdict['cndfs_time_' + nbt] =\
+                                                get_time('-C', nbt, rowdict)
 
-        csvline = csvline.replace('yes', '1').replace('no','-1')\
-                         .replace('maybe', '0').replace('EMPTY', '1')\
-                         .replace('NONEMPT', '0')
-        with open(outfile, 'a+') as f:
-            f.write(csvline + '\n')
+                if not options.timeonly:
+                    p = subprocess.Popen([options.exe, '-x', '-t',
+                                          '-m', rowdict['model'],
+                                          '-f', rowdict['formula']],
+                                          stdout=subprocess.PIPE)
+                    p.wait()
+                    output = p.stdout.readlines()
+                    del p
+                    for i in range(len(output)):
+                        line = str(output[i])
+                        if 'incidence' in line:
+                            data = output[i + 1].decode()[:-1].split(',')
+
+                            rowdict['incidence_in'] = data[0]
+                            rowdict['incidence_out'] = data[1]
+                            rowdict['average_incidence_ratio'] = data[2]
+                            rowdict['repeated_transitions'] = data[3]
+
+                        if 'terminal' in line:
+                            data = output[i + 1].decode()[:-1].split(',')
+
+                            rowdict['terminal'] = data[0]
+                            rowdict['weak'] = data[1]
+                            rowdict['inherently_weak'] = data[2]
+                            rowdict['very_weak'] = data[3]
+                            rowdict['complete'] = data[4]
+                            rowdict['universal'] = data[5]
+                            rowdict['unambiguous'] = data[6]
+                            rowdict['semi_deterministic'] = data[7]
+                            rowdict['stutter_invariant'] = data[8]
+
+                        if 'sccs' in line:
+                            rowdict['sccs'] = output[i + 1].decode()\
+                                                           .split(',')[-3]
+                            break
+                writer.writerow(rowdict)
+
+if __name__ == '__main__':
+    options, filein = get_options()
+    if options.fileout is None:
+        basename = os.path.basename(filein)
+        options.fileout = 'features_' + basename
+    fieldnames = get_fields(options)
+    extract(options, filein, fieldnames)
